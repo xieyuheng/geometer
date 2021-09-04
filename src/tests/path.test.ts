eb4614ebe6ede1761be9f915d016cd13eddd7862
Xie Yuheng <xyheme@gmail.com>
2021-09-05 02:39:09 +0800

    up

--- src/tests/path.test.ts
+++ src/tests/path.test.ts
@@ -1,5 +1,34 @@
-import ld from ".."
+import ld, { Complex } from ".."
 
+{
+  const torus = new Complex()
 
+  const origin = torus.node()
 
-// ld.path()
+  const toro = torus.edge(origin, origin)
+  const polo = torus.edge(origin, origin)
+
+  const spoke = torus.face({
+    a: toro,
+    b: polo,
+    c: toro.inverse,
+    d: polo.inverse,
+  })
+
+  console.log(torus.repr())
+
+  const path = ld.path([
+    toro,
+    polo,
+    toro,
+    ld.path([toro, toro, toro]),
+    ld.path([toro, toro, toro]),
+    ld.path([
+      toro,
+      toro,
+      toro,
+      ld.path([toro, toro, toro]),
+      ld.path([toro, toro, toro]),
+    ]),
+  ])
+}
---
+++ src/tests/surface.test.ts
@@ -0,0 +1,30 @@
+import ld, { Complex } from ".."
+
+{
+  const torus = new Complex()
+
+  const origin = torus.node()
+
+  const toro = torus.edge(origin, origin)
+  const polo = torus.edge(origin, origin)
+
+  const spoke = torus.face({
+    a: toro,
+    b: polo,
+    c: toro.inverse,
+    d: polo.inverse,
+  })
+
+  // const s1 = ld
+  //   .surface([
+  //     spoke.o({ a: "a1", b: "b", c: "c", d: "d" }),
+  //     spoke.o({ a: "a2", b: "b", c: "c", d: "d" }),
+  //   ])
+  //   .remain(["a1", "a2"])
+
+  // ld.surface([
+  //   s1.o({ a1: "a1", a2: "a" }),
+  //   s1.o({ a1: "a", a2: "b" }),
+  //   s1.o({ a1: "b", a2: "a2" }),
+  // ])
+}
